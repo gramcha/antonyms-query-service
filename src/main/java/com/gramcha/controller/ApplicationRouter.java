cb89606368ab428f7a821960c8dd5bb90a4bff3e
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-10-26 16:39:10 +0530

    git ignore update

---
+++ src/main/java/com/gramcha/controller/ApplicationRouter.java
@@ -0,0 +1,29 @@
+package com.gramcha.controller;
+
+import com.gramcha.config.ApplicationConfiguration;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by gramachandran on 26/10/18.
+ */
+@Component
+public class Route {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    @Autowired
+    private ApplicationConfiguration applicationConfiguration;
+
+//    public Router getRouter(Vertx vertx) {
+//        Router router = Router.router(vertx);
+//        router.route().handler(BodyHandler.create());
+//        router.get("/antonyms/:word").handler(this::antonyms);
+//        router.get("/info").handler(this::info);
+//        return router;
+//    }
+}
