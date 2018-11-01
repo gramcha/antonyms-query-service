64c5b1ab0d48340a432bc2ab5a56250785718bc5
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-11-01 16:46:16 +0530

    hystrix and docker support added

--- src/main/java/com/gramcha/antonymsqueryservice/AntonymsQueryServiceApplication.java
+++ src/main/java/com/gramcha/antonymsqueryservice/AntonymsQueryServiceApplication.java
@@ -5,7 +5,9 @@ import io.vertx.core.Vertx;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 
 import javax.annotation.PostConstruct;
@@ -13,6 +15,7 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 @EnableDiscoveryClient     // Register this service as uerka client to eureka server
 @ComponentScan(basePackages = "com.gramcha.*")
+//@EnableCircuitBreaker      // Enable circuit breakers
 public class AntonymsQueryServiceApplication {
 
     @Autowired
